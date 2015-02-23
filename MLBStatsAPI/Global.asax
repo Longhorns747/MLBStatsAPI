01694dadd2b07e871ec32777f1dba621f41e98ff
Ethan Shernan <eshernan3@gatech.edu>
2015-02-23 16:32:08 -0500

    Initial Commit

---
+++ MLBStatsAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MLBStatsAPI.WebApiApplication" Language="C#" %>
---
+++ MLBStatsAPI/Models/TeamYear.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MLBStatsAPI.Models
+{
+    public class TeamYear
+    {
+        public int year { get; set; }
+        public int W { get; set; }
+        public int L { get; set; }
+        public int R { get; set; }
+    }
+}
