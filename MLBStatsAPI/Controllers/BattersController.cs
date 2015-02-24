1604834288a168d400cb216cd53614c53bbca76c
Ethan Shernan <eshernan3@gatech.edu>
2015-02-23 21:18:58 -0500

    Added OBP calculation

--- MLBStatsAPI/Controllers/BattersController.cs
+++ MLBStatsAPI/Controllers/BattersController.cs
@@ -13,8 +13,9 @@ namespace MLBStatsAPI.Controllers
 {
     public class BattersController : ApiController
     {
-        string[] STATS = { "teamID", "G", "AB", "H", "HR", "RBI", 
+        string[] STATS = { "teamID", "G", "AB", "H", "HR", "RBI", "SB",
                              @"CAST(ROUND((H + 0.0) / (AB + 0.0), 3) as decimal(38, 3)) AS AVG",
+                             @"CAST(ROUND((H+BB+HBP + 0.0) / (AB+BB+HBP+COALESCE(SF,0) + 0.0), 3) as decimal(38, 3)) AS OBP",
                              @"CAST(ROUND(((H + ""2B"" + 2 * ""3B"" + 3 * HR) + 0.0) / (AB + 0.0), 3) as decimal(38, 3)) AS SLG" };
         public IHttpActionResult GetTeam(string firstName, string lastName)
         {
