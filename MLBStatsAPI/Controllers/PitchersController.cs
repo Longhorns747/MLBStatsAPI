9eeadf56b79401b6106875b8ddcefb0364cb4caf
Ethan Shernan <eshernan3@gatech.edu>
2015-02-26 13:15:37 -0500

    Added BABIP for pitchers

--- MLBStatsAPI/Controllers/PitchersController.cs
+++ MLBStatsAPI/Controllers/PitchersController.cs
@@ -16,9 +16,10 @@ namespace MLBStatsAPI.Controllers
         public IHttpActionResult GetPitcher(string firstName, string lastName)
         {
             stats = new string[] { "teamID", "G", "W", "L", "CAST(IPOuts / 3.0 as decimal(38,1)) AS IP", "GS", "SO", "BB",
-                             "CG", "SHO", "SV", "ERA", "BAOpp", 
+                             "CG", "SHO", "SV", "ERA", "BAOpp",
                              @"CAST((SO*9.0) / CAST(IPOuts / 3.0 as decimal(38,1)) as decimal(38, 2)) AS Kper9",
-                             @"CAST((BB*9.0) / CAST(IPOuts / 3.0 as decimal(38,1)) as decimal(38, 2)) AS BBper9"
+                             @"CAST((BB*9.0) / CAST(IPOuts / 3.0 as decimal(38,1)) as decimal(38, 2)) AS BBper9",
+                             @"CAST((H - HR + 0.0)/((BFP - HBP - IBB - BB - SH) - SO - HR + SF) as decimal(38, 3)) AS BABIP"
                          };
             table = "pitching";
             
